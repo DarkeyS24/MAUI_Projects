fcf25b37e02615236cb72178c6a6106f5c08fc8e
DarkeyS24 <angel.lperez.m16@gmail.com>
2025-02-18 16:58:37 -0300

    Adicionar arquivos de projeto.

---
+++ MAUI_Projects/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace MAUI_Projects
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnGenerateLuckyNumbers(object sender, EventArgs e)
+        {
+            NameApp.IsVisible = false;
+            ContainerLuckyNumbers.IsVisible = true;
+            var listNumbers = GenerateLuckyNumbers();
+            LuckyNumber01.Text = listNumbers.ElementAt(0).ToString("D2");
+            LuckyNumber02.Text = listNumbers.ElementAt(1).ToString("D2");
+            LuckyNumber03.Text = listNumbers.ElementAt(2).ToString("D2");
+            LuckyNumber04.Text = listNumbers.ElementAt(3).ToString("D2");
+            LuckyNumber05.Text = listNumbers.ElementAt(4).ToString("D2");
+            LuckyNumber06.Text = listNumbers.ElementAt(5).ToString("D2");
+        }
+
+        private SortedSet<int> GenerateLuckyNumbers()
+        {
+            var listNumbers = new SortedSet<int>();
+            while (listNumbers.Count < 6)
+            {
+                var random = new Random();
+                var luckyNumber = random.Next(1, 60);
+                listNumbers.Add(luckyNumber);
+            }
+            return listNumbers;
+        }
+    }
+}
