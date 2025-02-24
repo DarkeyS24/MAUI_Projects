59b0bf5d9fa9147bf6f71b632ed8241f08c1fe87
DarkeyS24 <angel.lperez.m16@gmail.com>
2025-02-24 11:50:40 -0300

    New app (MAUI Gallery)

--- AppMAUIGallery/Repositories/CategoryRepository.cs
+++ AppMAUIGallery/Repositories/CategoryRepository.cs
@@ -17,10 +17,33 @@ namespace AppMAUIGallery.Repositories
             List<Category> categories = new List<Category>();
             categories.Add(new Category { 
                 Name="Layout",
-                Components = new List<Component> { new Component { 
-                    Name="StackLayout",
-                    Description="Organização sequencial dos elementos.",
-                    Page = typeof(StackLayoutPage)} }
+                Components = new List<Component> 
+                { 
+                     new Component 
+                     { 
+                        Name="StackLayout",
+                        Description="Organização sequencial dos elementos.",
+                        Page = typeof(StackLayoutPage)
+                     },
+                     new Component
+                     {
+                        Name="Grid",
+                        Description="Organiza os elementos dentro de uma tabela.",
+                        Page = typeof(GridLayoutPage)
+                     },
+                     new Component 
+                     {
+                         Name="AbsoluteLayout",
+                         Description="Liberdade total para posicionar e dimensionar os elementos na tela.",
+                         Page = typeof(AbsoluteLayoutPage)
+                     },
+                     new Component
+                     {
+                         Name="FlexLayout",
+                         Description="Organiza os elementos de forma sequencial com muitas opções de personalização",
+                         Page = typeof(FlexLayoutPage)
+                     }
+                }
             });
             return categories; 
         }
