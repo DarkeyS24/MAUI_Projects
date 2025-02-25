de815d5ad4e477d1de7d7780075f284ae73a020d
DarkeyS24 <angel.lperez.m16@gmail.com>
2025-02-25 17:02:36 -0300

    Almost there

--- AppJogoForca/MainPage.xaml.cs
+++ AppJogoForca/MainPage.xaml.cs
@@ -1,25 +1,81 @@
-﻿namespace AppJogoForca
+using AppJogoForca.Libraries.Text;
+using AppJogoForca.Models;
+using AppJogoForca.Repositories;
+
+namespace AppJogoForca;
+
+public partial class MainPage : ContentPage
 {
-    public partial class MainPage : ContentPage
+	private Word word;
+	private int misses = 0;
+
+	public MainPage()
+	{
+		
+		InitializeComponent();
+
+		ResetScreen();
+	}
+
+	private async void OnButtonClicked(object sender, EventArgs e)
+	{
+		Button button = (Button)sender;
+        button.IsEnabled = false;
+		string letter = button.Text;
+		var positions =word.Text.AllIndexesOf(letter);
+
+		if(positions.Count == 0)
+		{
+			misses++;
+			imgMain.Source = ImageSource.FromFile($"forca{misses + 1}.png");
+			button.Style = App.Current.Resources.MergedDictionaries.ElementAt(1)["Misses"] as Style;
+
+			if (misses == 6)
+			{
+				await DisplayAlert("You lose!", "Wanna try again?", "New game");
+				ResetScreen();
+			}
+            return;
+		}
+
+		foreach(int position in positions)
+		{
+			lblText.Text = lblText.Text.Remove(position, 1).Insert(position, letter);
+            button.Style = App.Current.Resources.MergedDictionaries.ElementAt(1)["Hits"] as Style;
+        }
+	}
+
+    private void ResetScreen()
     {
-        int count = 0;
-
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+		ResetKeyboard();
+		ResetMisses();
+		GenerateNewWord();
     }
+	private void GenerateNewWord()
+	{
+		var repository = new WordRepository();
+        word = repository.GetRamdomWord();
 
+        lblTips.Text = word.Tips;
+        lblText.Text = new string('_', word.Text.Length);
+	}
+	private void ResetMisses()
+	{
+		misses = 0;
+		imgMain.Source = ImageSource.FromFile("forca1.png");
+	}
+	private void ResetKeyboard()
+	{
+		ResetVirtualKey((HorizontalStackLayout)keyboardContainer.Children[0]);
+		ResetVirtualKey((HorizontalStackLayout)keyboardContainer.Children[1]);
+		ResetVirtualKey((HorizontalStackLayout)keyboardContainer.Children[2]);
+	}
+	private void ResetVirtualKey(HorizontalStackLayout h)
+	{
+		foreach(Button button in h.Children)
+		{
+			button.IsEnabled = true;
+            button.Style = null;
+        }
+	}
 }
---
+++ AppJogoForca/Repositories/WordRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppJogoForca.Models;
+
+namespace AppJogoForca.Repositories
+{
+    internal class WordRepository
+    {
+        private List<Word> words;
+
+        public WordRepository() 
+        {
+            words = new List<Word>();
+            words.Add(new Word("Nome","Maria".ToUpper()));
+            words.Add(new Word("Vegetal","Cenoura".ToUpper()));
+            words.Add(new Word("Fruta","Abacate".ToUpper()));
+            words.Add(new Word("Tempero","Nordestino".ToUpper()));
+            words.Add(new Word("Tempero","Baiano".ToUpper()));
+        }
+
+        public Word GetRamdomWord()
+        {
+            int maxValue = words.Count - 1;
+            int minValue = 0;
+            Random random = new Random();
+            int num = random.Next(minValue, maxValue);
+            return words[num];
+        }
+    }
+}
