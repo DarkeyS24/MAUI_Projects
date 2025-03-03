d61bd27539bb92635713acb554bf007655c60f85
DarkeyS24 <angel.lperez.m16@gmail.com>
2025-03-03 09:27:45 -0300

    Movie list

--- AppMAUIGallery/Repositories/CategoryRepository.cs
+++ AppMAUIGallery/Repositories/CategoryRepository.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AppMAUIGallery.Models;
+using AppMAUIGallery.Views.Cells;
 using AppMAUIGallery.Views.Components.Forms;
 using AppMAUIGallery.Views.Components.Mains;
 using AppMAUIGallery.Views.Layouts;
+using AppMAUIGallery.Views.Lists;
 using AppMAUIGallery.Views.Visuals;
 
 namespace AppMAUIGallery.Repositories
@@ -181,6 +183,61 @@ namespace AppMAUIGallery.Repositories
                         Name="Picker",
                         Description="Selecionar um item da lista.",
                         Page=typeof(PickerPage)
+                    }
+                }
+            });
+            categories.Add(new Category
+            {
+                Name = "Células",
+                Components = new List<Component>
+                {
+                    new Component()
+                    {
+                        Name="TextCell",
+                        Description="Apresenta até duas 'labels' onde uma é destinada ao titulo e a outra a descrição.",
+                        Page=typeof(TextCellPage)
+                    },
+                    new Component()
+                    {
+                        Name="ImageCell",
+                        Description="Apresenta uma imagem com até duas 'labels' onde uma é destinada ao titulo e a outra a descrição.",
+                        Page=typeof(ImageCellPage)
+                    },
+                    new Component()
+                    {
+                        Name="SwitchCell",
+                        Description="Apresenta uma label em conjunto com um switch.",
+                        Page=typeof(SwitchCellPage)
+                    },
+                    new Component()
+                    {
+                        Name="EntryCell",
+                        Description="Apresenta uma label em conjunto com um entry.",
+                        Page=typeof(EntryCellPage)
+                    },
+                    new Component()
+                    {
+                        Name="ViewCell",
+                        Description="Permite criar a nossa célula com layout personalizado.",
+                        Page=typeof(ViewCellPage)
+                    }
+                }
+            });
+            categories.Add(new Category{
+                Name="Listas e Coleções",
+                Components = new List<Component> 
+                {
+                    new Component() 
+                    {
+                        Name="TableView",
+                        Description="Apresenta células em linhas separadas e permite agrupar por seção",
+                        Page = typeof(TableViewPage)
+                    },
+                    new Component() 
+                    {
+                        Name="Picker",
+                        Description="Apresenta uma lista de seleção única",
+                        Page = typeof(PickerListPage)
                     }
                 }
             });
---
+++ AppMAUIGallery/Views/Lists/PickerListPage.xaml.cs
@@ -0,0 +1,21 @@
+using AppMAUIGallery.Views.Lists.Models;
+
+namespace AppMAUIGallery.Views.Lists;
+
+public partial class PickerListPage : ContentPage
+{
+	public PickerListPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+		pickerControl.ItemsSource = MovieList.GetList();
+    }
+
+    private void pickerControl_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+}
