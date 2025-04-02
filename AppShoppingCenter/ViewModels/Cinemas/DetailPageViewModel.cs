554738acfac1b83b8a44e3f0c7d15cb798694d2b
DarkeyS24 <angel.lperez.m16@gmail.com>
2025-04-02 11:26:21 -0300

    Cinema Page

--- AppShoppingCenter/ViewModels/Cinemas/DetailPageViewModel.cs
+++ AppShoppingCenter/ViewModels/Cinemas/DetailPageViewModel.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AppShoppingCenter.Models;
+using CommunityToolkit.Maui.Views;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace AppShoppingCenter.ViewModels.Cinemas
 {
-    class DetailPageViewModel
+    [QueryProperty(nameof(Movie),"movie")]
+    public partial class DetailPageViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private Movie movie;
     }
 }
--- AppShoppingCenter/Views/Cinemas/DetailPage.xaml.cs
+++ AppShoppingCenter/Views/Cinemas/DetailPage.xaml.cs
@@ -1,9 +1,47 @@
+using CommunityToolkit.Maui.Views;
+
 namespace AppShoppingCenter.Views.Cinemas;
 
 public partial class DetailPage : ContentPage
 {
+	private bool firstTime = false;
 	public DetailPage()
 	{
 		InitializeComponent();
 	}
+
+    private async void PlayPause(object sender, TappedEventArgs e)
+    {
+		player.IsVisible = true;
+		var btn = (Image)sender;
+		if (firstTime == false)
+		{
+            double mediaWidth = player.Width;
+            double mediaHeight = player.Height;
+            double targetX = 300 - ((mediaWidth / 2) - btn.Width);
+            double targetY = 160 - ((mediaHeight / 2) - btn.Height);
+
+            playPauseBtn.IsVisible = false;
+            await playPauseBtn.TranslateTo(-targetX, targetY, 500);
+            playPauseBtn.IsVisible = true;
+            firstTime = true;
+		}
+
+		if (player.CurrentState == CommunityToolkit.Maui.Core.Primitives.MediaElementState.Playing)
+		{
+			player.Pause();
+			playPauseBtn.Source = ImageSource.FromFile("play.png");
+		}
+		else
+		{
+			player.Play();
+			playPauseBtn.Source = ImageSource.FromFile("pause.png");
+		}
+    }
+
+    private void OnTapCloseDetailPage(object sender, TappedEventArgs e)
+    {
+        player.Pause();
+        Shell.Current.GoToAsync("..");
+    }
 }
