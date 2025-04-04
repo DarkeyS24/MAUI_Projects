21c5f4368785a1f02397f6d92221d4a7a481bdf9
DarkeyS24 <angel.lperez.m16@gmail.com>
2025-04-04 08:45:51 -0300

    Camera Page (Bug Fixed{Camera doesn't work})

--- AppShoppingCenter/Views/Tickets/CameraPage.xaml.cs
+++ AppShoppingCenter/Views/Tickets/CameraPage.xaml.cs
@@ -9,5 +9,26 @@ public partial class CameraPage : ContentPage
 	public CameraPage()
 	{
 		InitializeComponent();
-	}
+		//cameraView.WidthRequest = DeviceDisplay.MainDisplayInfo.Width;
+		//cameraView.HeightRequest = DeviceDisplay.MainDisplayInfo.Height;
+    }
+
+    public void BarcodesDetected(object sender, BarcodeDetectionEventArgs e)
+    {
+        var ticketNumber = e.Results[0].Value;
+        var service = App.Current.Handler.MauiContext.Services.GetService<TicketService>();
+        var ticket = service.GetTicket(ticketNumber);
+        if (ticket == null)
+        {
+            //Mensagem de alerta
+            App.Current.MainPage.DisplayAlert("Ticket não encontrado", $"Não localizamos o ticket com o número {ticketNumber}", "OK");
+            return;
+        }
+        //Navegar para a página pay
+        var param = new Dictionary<string, object>()
+                {
+                            {"ticket", ticket}
+                        };
+        MainThread.BeginInvokeOnMainThread(async () => { await Shell.Current.GoToAsync("../pay", param); });
+    }
 }
